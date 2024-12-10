966b3547f5acf9e143f5d1556b3da279af542512
ADOBENET\ericlee <ericlee@adobe.com>
2024-12-10 18:33:53 +0900

    second week session homework - 1 - jslint

--- blocks/video/video.js
+++ blocks/video/video.js
@@ -1,13 +1,12 @@
-
 export default async function decorate(block) {
   const videoTags = block.querySelectorAll('a');
 
   [...videoTags].forEach((row) => {
-    const video = document.createElement("iframe");
+    const video = document.createElement('iframe');
     video.src = row.href;
-    video.width = "420";
-    video.height = "315";
-    video.frameBorder = "0";
+    video.width = '420';
+    video.height = '315';
+    video.frameBorder = '0';
     video.allowFullscreen = true;
     block.append(video);
   });
