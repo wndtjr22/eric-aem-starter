6d3fc968d17c3523c6335a71ff5480b4782e8470
ADOBENET\ericlee <ericlee@adobe.com>
2024-12-10 12:05:54 +0900

    second week session homework - 2

--- blocks/black-friday-carousel/black-friday-carousel.js
+++ blocks/black-friday-carousel/black-friday-carousel.js
@@ -1,26 +1,6 @@
-
 export default async function decorate(block) {
-  console.log("ERIC-BLACK-FRIDAY-BLOCK run decorate function");
+  console.log('ERIC-BLACK-FRIDAY-BLOCK run decorate function');
   let slideIndex = 1;
-
-  showSlides(slideIndex);
-  addNextBtn(block);
-
-  function addNextBtn(block){
-    // add next button
-    const prev = document.createElement('a');
-    prev.className = "prev-btn";
-    prev.innerText = "<";
-    prev.onclick = function() { plusSlides(-1);};
-
-    const next = document.createElement('a');
-    next.className = "next-btn"
-    next.innerText = ">";
-    next.onclick = function() { plusSlides(1);};
-
-    block.append(prev);
-    block.append(next);
-  }
 
   function showSlides(n) {
     let i;
@@ -28,14 +8,34 @@ export default async function decorate(block) {
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+      slides[i].style.display = 'none';
     }
-    slides[slideIndex-1].style.display = "block";
+    slides[slideIndex-1].style.display = 'block';
   }
 
+  function addNextBtn(block){
+    // add next button
+    const prev = document.createElement('a');
+    prev.className = 'prev-btn';
+    prev.innerText = '<';
+    prev.onclick = function() { plusSlides(-1);};
+
+    const next = document.createElement('a');
+    next.className = 'next-btn'
+    next.innerText = '>';
+    next.onclick = function() { plusSlides(1);};
+
+    block.append(prev);
+    block.append(next);
+  }
   function plusSlides(n) {
     showSlides(slideIndex += n);
   }
+
+  showSlides(slideIndex);
+  addNextBtn(block);
+
+
 }
 
 
